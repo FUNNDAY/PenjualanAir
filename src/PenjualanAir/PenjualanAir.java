c007ed7e9c51174b191f187b4b6e0d0bd10e2bdd
FUNNGOD0 <okrizal98@gmail.com>
2025-01-05 10:25:38 +0800

    1

---
+++ src/PenjualanAir/PenjualanAir.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package PenjualanAir;
+    import configDatabase.configDB;
+    import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ACER
+ */
+public class PenjualanAir {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        new gui.mainFrame().setVisible(true);
+        
+    }
+    
+}
